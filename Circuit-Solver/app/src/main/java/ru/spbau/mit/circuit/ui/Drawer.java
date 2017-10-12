afe9b4349e14b426ce5beb8d538aa6825faa69fe
Kirakosian Aleksander <kair1998@gmail.com>
2017-10-12 15:50:29 +0300

    Drawer revert

--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/ui/Drawer.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/ui/Drawer.java
@@ -4,22 +4,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
-import java.util.ArrayList;
-
-import ru.spbau.mit.circuit.model.Element;
-
-import static ru.spbau.mit.circuit.MainActivity.controller;
-
 public class Drawer {
-    public static ArrayList<Drawable> drawables = new ArrayList<>();
     public static int cellSize = 50;
-    public static Paint elementsPaint;
-
-    static {
-        elementsPaint = new Paint();
-        elementsPaint.setColor(Color.RED);
-        elementsPaint.setStrokeWidth(5);
-    }
 
     public static void drawBackground(Canvas canvas) {
         canvas.drawColor(Color.BLACK);
@@ -30,16 +16,6 @@ public class Drawer {
         }
         for (int i = 0; i < canvas.getWidth(); i += cellSize) {
             canvas.drawLine(i, 0, i, canvas.getHeight(), paint);
-        }
-    }
-
-    public static void drawEverything(Canvas canvas) {
-        ArrayList<Element> elements = controller.getElements();
-
-
-        drawBackground(canvas);
-        for (Drawable element : drawables) {
-            element.draw(canvas);
         }
     }
 }
