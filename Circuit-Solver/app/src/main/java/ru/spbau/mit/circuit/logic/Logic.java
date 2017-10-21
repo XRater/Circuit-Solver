7811b15c52e5418fe1a0020e653020ad0610f06a
Kirakosian Aleksander <kair1998@gmail.com>
2017-10-21 22:00:50 +0300

    simple logic for compilation

--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/logic/Logic.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/logic/Logic.java
@@ -1,6 +1,19 @@
 package ru.spbau.mit.circuit.logic;
 
+import ru.spbau.mit.circuit.controler.Controller;
+import ru.spbau.mit.circuit.controler.View;
+import ru.spbau.mit.circuit.model.Model;
 
-public class Logic {
+public class Logic implements View {
 
+    private final Controller controller;
+
+    public Logic(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void update(Model model) {
+
+    }
 }
