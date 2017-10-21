51111d6b5a7249349fb8a580cacfe9237b60e93d
Kirakosian Aleksander <kair1998@gmail.com>
2017-10-21 21:58:01 +0300

    View interface & simple Controler

--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/controler/Controller.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/controler/Controller.java
@@ -3,12 +3,25 @@ package ru.spbau.mit.circuit.controler;
 
 import java.util.ArrayList;
 
+import ru.spbau.mit.circuit.logic.Logic;
 import ru.spbau.mit.circuit.model.Element;
+import ru.spbau.mit.circuit.model.Model;
 
 public class Controller {
-    private ArrayList<Element> elements = new ArrayList<>();
 
-    public ArrayList<Element> getElements() {
-        return elements;
+    private Model model;
+    private final ArrayList<View> views;
+
+    public Controller() {
+        views = new ArrayList<>();
+        views.add(new Logic(this));
+        //TODO add UI here
     }
+
+    public void updateView() {
+        for (View view : views)
+            view.update(model);
+    }
+
+    //TODO events from views
 }
--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/model/Model.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/model/Model.java
@@ -1,5 +1,12 @@
 package ru.spbau.mit.circuit.model;
 
 
+import java.util.ArrayList;
+
 public class Model {
+    private ArrayList<Element> elements = new ArrayList<>();
+
+    public ArrayList<Element> getElements() {
+        return elements;
+    }
 }
