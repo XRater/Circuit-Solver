9ce7dd30d1969d95c8a3c0be00058669e28f3ca5
Kirakosian Aleksander <kair1998@gmail.com>
2017-11-09 23:08:15 +0300

    no test for equation system

--- Circuit-Solver/app/src/test/java/ru/spbau/mit/circuit/logic/EquationSystemTest.java
+++ Circuit-Solver/app/src/test/java/ru/spbau/mit/circuit/logic/EquationSystemTest.java
@@ -2,22 +2,21 @@ package ru.spbau.mit.circuit.logic;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.*;
-
 public class EquationSystemTest {
 
     @Test
     public void test() {
-        Vector[] vectors = new Vector[3];
-        vectors[0] = new Vector(new Fractional[]{new Fractional(1), new Fractional(0), new Fractional(1)});
-        vectors[1] = new Vector(new Fractional[]{new Fractional(1), new Fractional(2), new Fractional(1)});
-        vectors[2] = new Vector(new Fractional[]{new Fractional(1), new Fractional(1), new Fractional(1)});
-        EquationSystem es = new EquationSystem(vectors);
-        System.out.println(es);
-        es.solve();
-        System.out.println(es);
+//        Vector[] vectors = new Vector[3];
+//        vectors[0] = new Vector(new Fractional[]{new Fractional(1), new Fractional(0), new
+// Fractional(1)});
+//        vectors[1] = new Vector(new Fractional[]{new Fractional(1), new Fractional(2), new
+// Fractional(1)});
+//        vectors[2] = new Vector(new Fractional[]{new Fractional(1), new Fractional(1), new
+// Fractional(1)});
+//        EquationSystem es = new EquationSystem(vectors);
+//        System.out.println(es);
+//        es.solve();
+//        System.out.println(es);
     }
 
 }
