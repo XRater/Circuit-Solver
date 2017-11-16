198914dc655540c8f4fb1e9c9534f6942ee05f07
AndreySmirdin <andrey.smirdin@mail.ru>
2017-11-16 12:10:49 +0300

    formatting of currents.

--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/ui/Drawer.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/ui/Drawer.java
@@ -66,7 +66,8 @@ public class Drawer {
     public static void showCurrents(MyCanvas canvas) {
         for (Drawable d : drawables) {
             Element e = (Element) d;
-            canvas.drawText(Math.abs(e.getCurrent()) + "A", d.x() - CELL_SIZE / 4, d.y() -
+            String current = String.format("%.2f", Math.abs(e.getCurrent()));
+            canvas.drawText(current + "A", d.x() - CELL_SIZE / 4, d.y() -
                             CELL_SIZE / 3 * 2,
                     elementsPaint);
         }
