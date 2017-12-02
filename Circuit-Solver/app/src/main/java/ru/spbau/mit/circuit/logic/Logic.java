085fa71adc28fee496f7ca99cfb04f528f8300ab
Kirakosian Aleksander <kair1998@gmail.com>
2017-12-03 00:23:47 +0300

    working on grap

--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/logic/Logic.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/logic/Logic.java
@@ -6,8 +6,6 @@ import ru.spbau.mit.circuit.controler.Controller;
 import ru.spbau.mit.circuit.logic.graph.ConnectedGraph;
 import ru.spbau.mit.circuit.logic.graph.Graph;
 import ru.spbau.mit.circuit.model.Model;
-import ru.spbau.mit.circuit.model.elements.Resistor;
-import ru.spbau.mit.circuit.model.point.Point;
 
 public class Logic {
 
@@ -17,18 +15,18 @@ public class Logic {
         this.controller = controller;
     }
 
+
     public void calculateCurrents(Model model) throws CircuitShortingException {
         Graph g = new Graph(model);
         List<ConnectedGraph> components = g.decompose();
         for (ConnectedGraph component : components) {
             System.out.println(component);
-            //            component.solve();
-//            component.setCurrents();
+//                    component.solve();
+            component.setCurrents();
         }
     }
 
     public static void main(String[] args) {
-        Model m = new Model();
-        m.addElement(new Resistor(new Point(10, 10), new Point(10, 20)));
+
     }
 }
