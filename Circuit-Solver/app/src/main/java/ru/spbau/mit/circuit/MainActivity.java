e27c6001027f88c6ed1017c9ea5fa7347845e71c
Kirakosian Aleksander <kair1998@gmail.com>
2017-12-15 00:57:04 +0300

    load and files

--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/MainActivity.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/MainActivity.java
@@ -18,6 +18,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import ru.spbau.mit.circuit.controler.Controller;
+import ru.spbau.mit.circuit.storage.Converter;
 import ru.spbau.mit.circuit.storage.DriveStorage;
 import ru.spbau.mit.circuit.ui.NewCircuitActivity;
 import ru.spbau.mit.circuit.ui.UI;
@@ -46,8 +47,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoadCircuit(View view) {
-        Intent intent = new Intent(MainActivity.this, LoadActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(MainActivity.this, LoadActivity.class);
+//        startActivity(intent);
+        System.out.println(Thread.currentThread());
+        Thread thread = new Thread(() ->
+                System.out.println(controller.getCircuits(Converter.Mode.DRIVE)));
+        thread.start();
     }
 
     public void onSettings(View view) {
--- Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/storage/DriveStorage.java
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/storage/DriveStorage.java
@@ -6,10 +6,23 @@ import android.app.Activity;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveClient;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.DriveResourceClient;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataBuffer;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 
 import java.io.ByteArrayInputStream;
-import java.util.Collection;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import ru.spbau.mit.circuit.MainActivity;
 
@@ -23,7 +36,6 @@ public class DriveStorage implements Storage {
     public DriveStorage(Activity mainActivity) {
         this.activity = (MainActivity) mainActivity;
         activity.initDrive(this);
-//        initializeDriveClient(activity.getSignedInAccount());
     }
 
     public void initializeDriveClient(GoogleSignInAccount signInAccount) {
@@ -33,17 +45,54 @@ public class DriveStorage implements Storage {
     }
 
     @Override
-    public void saveToNewFile(byte[] stream, String filename) {
+    public void save(byte[] bytes, String filename) {
+        final Task<DriveFolder> rootFolderTask = mDriveResourceClient.getAppFolder();
+        final Task<DriveContents> createContentsTask = mDriveResourceClient.createContents();
+        Tasks.whenAll(rootFolderTask, createContentsTask)
+                .continueWithTask(task -> {
+                    DriveFolder parent = rootFolderTask.getResult();
+                    DriveContents contents = createContentsTask.getResult();
+                    OutputStream outputStream = contents.getOutputStream();
+                    outputStream.write(bytes);
 
+                    MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                            .setTitle(filename)
+                            .setMimeType("text/plain")
+                            .setStarred(true)
+                            .build();
+
+                    return mDriveResourceClient.createFile(parent, changeSet, contents);
+                });
     }
 
     @Override
-    public Collection<String> getFiles() {
-        return null;
+    public List<String> getCircuits() {
+        Query query = new Query.Builder()
+                .addFilter(Filters.eq(SearchableField.MIME_TYPE, "text/plain"))
+                .build();
+
+        Task<MetadataBuffer> queryTask = mDriveResourceClient.query(query);
+        List<String> circuitsNames = new ArrayList<>();
+
+        try {
+            Tasks.await(queryTask);
+            for (Metadata metadata : queryTask.getResult()) {
+                circuitsNames.add(metadata.getTitle());
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return circuitsNames;
     }
 
     @Override
-    public ByteArrayInputStream loadFromFile(String filename) {
+    public ByteArrayInputStream load(String filename) {
+//        mDriveClient.newOpenFileActivityIntentSender(openOptions);
+//        Task<DriveContents> openFileTask =
+//                mDriveResourceClient.openFile(filename, DriveFile.MODE_READ_ONLY);
         return null;
     }
 }
