1aaa54b1afce215c678b647db6499219f2cece12
Kirakosian Aleksander <kair1998@gmail.com>
2017-12-16 11:49:45 +0300

    load activity

---
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/StorageActivity.java
@@ -0,0 +1,20 @@
+package ru.spbau.mit.circuit;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class StorageActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+}
