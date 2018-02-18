d4a1d6112f8c5cea4cf9ca4ea2f8fc0b33dd2b1e
Kirakosian Aleksander <kair1998@gmail.com>
2018-02-18 14:44:26 +0300

    parametersInput

---
+++ Circuit-Solver/app/src/main/java/ru/spbau/mit/circuit/logic/math/variables/ResultValueVariable.java
@@ -0,0 +1,38 @@
+package ru.spbau.mit.circuit.logic.math.variables;
+
+
+import ru.spbau.mit.circuit.logic.math.algebra.Numerical;
+import ru.spbau.mit.circuit.logic.math.functions.Function;
+
+/**
+ * Function variable. May have any Numerical initial value.
+ */
+public class FunctionVariable extends Variable<Function> {
+
+    private Numerical initialValue;
+
+    public FunctionVariable() {
+        super();
+    }
+
+    @SuppressWarnings("unused")
+    public FunctionVariable(String name) {
+        super(name);
+    }
+
+    public Numerical initialValue() {
+        return initialValue;
+    }
+
+    public void setInitialValue(Numerical initialValue) {
+        this.initialValue = initialValue;
+    }
+
+    @Override
+    public String toString() {
+        if (name.equals("")) {
+            return "f" + id;
+        }
+        return name;
+    }
+}
